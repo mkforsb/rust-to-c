dd1e528be31ae34e906603b00e0829f9485073e1
Mikael Forsberg <mkforsb@gmail.com>
2025-02-15 12:43:38 +0100

    style: remove commented code

--- rust-app/build.rs
+++ rust-app/build.rs
@@ -1,6 +1,3 @@
 fn main() {
-    // println!("cargo:rustc-link-search=native=/home/lur/Desktop/rust-c-interop/rust-to-c/rust-app");
     println!("cargo:rustc-link-search=native=.");
-    // println!("cargo:rustc-link-lib=static=skitpatte");
-
 }
